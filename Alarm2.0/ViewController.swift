b5fae15a5b01d71fd68ee41f8820780d8bf8d554
Timmy-LUO <a220009996@gmail.com>
2022-02-25 09:26:10 +0800

    Initial Commit

---
+++ Alarm2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Alarm2.0
+//
+//  Created by 羅承志 on 2022/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
