74693c6c3067f51d4545b916cb4e581f235c1829
Timmy <a220009996@gmail.com>
2022-02-25 19:13:33 +0800

    add DatePicker header view

---
+++ Alarm2.0/Other/DatePickerHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  DatePickerHeaderView.swift
+//  Alarm2.0
+//
+//  Created by 羅承志 on 2022/2/25.
+//
+
+import UIKit
+
+class DatePickerHeaderView: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Alarm2.0/TableViewCell/AddAlarmTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AddAlarmTableViewCell.swift
+//  Alarm2.0
+//
+//  Created by 羅承志 on 2022/2/25.
+//
+
+import UIKit
+
+class AddAlarmTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
